ec8b45b11a20ec931b27f33bea68fa0a5e7d3898
kepldev <contact@kepl.org>
2018-03-01 00:38:59 -0500

    initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The KEPL developers
+//
+// This file is part of KEPL.
+//
+// KEPL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// KEPL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with KEPL.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid KEPL_NETWORK = { { 0xB1 ,0x1B, 0x0B, 0xB1 , 0xB1, 0xB0 , 0xB1, 0x1B, 0x1B, 0xB1, 0xB0, 0x0B, 0x1B, 0xB1, 0x0B, 0xB0} }; //Bender's nightmare
+}
