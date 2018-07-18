5b7fdaef21f5a841b0044cc9f2c4cb61ccb53799
LaurentDemanchelier <laurent@kepl.org>
2018-07-17 22:38:55 -0400

    set proper upgrade height and add checkpoint

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -84,7 +84,7 @@ const uint32_t KEY_IMAGE_CHECKING_BLOCK_INDEX                = 0;
 const uint32_t UPGRADE_HEIGHT_V2                             = 1;
 const uint32_t UPGRADE_HEIGHT_V3                             = 2;
 const uint32_t UPGRADE_HEIGHT_V4                             = 17000;
-const uint32_t UPGRADE_HEIGHT_V5                             = 50508;
+const uint32_t UPGRADE_HEIGHT_V5                             = 50509;
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
 const uint32_t UPGRADE_WINDOW                                = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
@@ -169,7 +169,7 @@ const CheckpointData CHECKPOINTS[] = {
   {5455, "a5799a8494d35735bc9bf09eec662f7e2f33912fe66e1da4d9f98d646b90927d"},
   {17001, "2fe2962dfef23c74b5dfc4380f3d76a3fbe62030c1d0e60ece6b090ab5020c11"},
   {30000, "e2ba73948df5aeb02e7978c0fde87b94c2da246e3e33326dda612d368863c0e5"},
-  {50509, "c5c5abd7545f589b1122c44a1d0c7b40418da6aff4137646dfcdd24e6452623e"}
+  {50510, "b2874c8b52bea617f880b47708678ada40ce85af2ddd3a3565be20544178d857"}
 };
 } // CryptoNote
 
